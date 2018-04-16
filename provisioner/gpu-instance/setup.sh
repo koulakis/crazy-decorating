039a907c0a5dff912fa5708686d1f464cb782f29
mariosk <marios.koulakis@gmail.com>
2018-04-15 21:46:00 -0400

    Downloads images and switches to g3 instances.

--- provisioner/gpu-instance/setup.sh
+++ provisioner/gpu-instance/setup.sh
@@ -6,4 +6,14 @@ set -e
 sudo locale-gen en_CA.UTF-8
 
 # Install common libraries
-(cd /home/ubuntu && source /home/ubuntu/anaconda3/bin//activate tensorflow_p36 && pip install gensim sklearn)
+(cd /home/ubuntu && source /home/ubuntu/anaconda3/bin//activate tensorflow_p36 && pip install sklearn scikit-image)
+
+# Download and unzip datasets
+mkdir /home/ubuntu/data
+(cd /home/ubuntu/ && wget https://s3-us-west-2.amazonaws.com/furniture-kaggle/train_images.zip)
+(cd /home/ubuntu/ && wget https://s3-us-west-2.amazonaws.com/furniture-kaggle/validation_images.zip)
+(cd /home/ubuntu/ && wget https://s3-us-west-2.amazonaws.com/furniture-kaggle/test_images.zip)
+
+(cd /home/ubuntu/ && unzip train_images.zip)
+(cd /home/ubuntu/ && unzip validation_images.zip)
+(cd /home/ubuntu/ && unzip test_images.zip)
